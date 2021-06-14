a485c30f78c4ea5f4bb6dbd7cb4b8034395b9634
malbeldas <maximilianoalbeldas@yahoo.com.ar>
2021-06-14 18:06:33 -0300

    navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from '../img/logo_B&B.jpg';
+
+class NavBar extends Component {
+    render() {
+        return (
+            <div className="container">
+                <nav className="nav-mobile">
+                    <a href="#"><img src={logo} className="logo" alt="Logo" /></a>
+                    <a href="#"><i className="fas fa-shopping-cart"></i></a>
+                </nav>
+            </div>
+        );
+      }
+}
+
+export default NavBar;

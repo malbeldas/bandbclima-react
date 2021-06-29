22fe73f3d61f3fbb5ab4abc7152e7c45870ed098
malbeldas <maximilianoalbeldas@yahoo.com.ar>
2021-06-29 10:58:43 -0300

    useState ItemList and categories

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,17 +1,17 @@
 import {ItemCount} from '../components/ItemCount'
 import {Link} from 'react-router-dom';
 
-function Item({stock, initial, id, title, description, price, pictureUrl}) {
+function Item({item, initial}) {
 
     return (
-        <div id={'itemCount_' + id} className="itemCount">
-            <Link to={`/product/${id}`}>
-                <h3 id={'itemCountTitle_' + id} className="itemCountTitle">{title}</h3>
-                <img src={pictureUrl} id={'itemCountImage_' + id} className="itemCountImage" alt="product" />
-                <p id={'itemCountDescription_' + id} className="itemCountDescription">{description}</p>
-                <h2 id={'itemCountPrice_' + id} className="itemCountPrice">$ {price}</h2>
+        <div id={'itemCount_' + item.id} className="itemCount">
+            <Link to={`/product/${item.id}`}>
+                <h3 id={'itemCountTitle_' + item.id} className="itemCountTitle">{item.title}</h3>
+                <img src={item.pictureUrl} id={'itemCountImage_' + item.id} className="itemCountImage" alt="product" />
+                <p id={'itemCountDescription_' + item.id} className="itemCountDescription">{item.description}</p>
+                <h2 id={'itemCountPrice_' + item.id} className="itemCountPrice">$ {item.price}</h2>
             </Link>            
-            <ItemCount stock={stock} initial={initial}/>
+            <ItemCount stock={item.stock} initial={initial}/>
         </div>
     );
 }
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,119 +1,13 @@
 import React from 'react'
 import {Item} from '../components/Item'
 
-let products = [{
-    id: 0,
-    title: 'Aire Acondicionado 1',
-    description: 'Aire acondicionado BGH split frío/calor 3000 frigorías blanco 220V BS35WCCR',
-    price: 55999,
-    pictureUrl: 'https://http2.mlstatic.com/D_NQ_NP_694117-MLA40282485103_122019-O.webp',
-    stock: 5,
-    category_id:0,
-},
-{
-    id: 1,
-    title: 'Aire Acondicionado 2',
-    description: 'Aire acondicionado Hyundai split inverter frío/calor 2924 frigorías blanco 220V HY6INV-3200FC',
-    price: 48590,
-    pictureUrl: 'https://http2.mlstatic.com/D_NQ_NP_997089-MLA40009455563_122019-O.webp',
-    stock: 0,
-    category_id:0,
-},
-{
-    id: 2,
-    title: 'Aire Acondicionado 3',
-    description: 'Aire acondicionado Midea split inverter frío/calor 4506 frigorías blanco 220V MSABIC-18H-01F',
-    price: 79999,
-    pictureUrl: 'https://http2.mlstatic.com/D_NQ_NP_790830-MLA41186141828_032020-O.webp',
-    stock: 4,
-    category_id:0,
-},
-{
-    id: 3,
-    title: 'Calefaccion 1',
-    description: 'Calefactor Central Conductos Goodman Gmp 125-25.750 Kcal/h',
-    price: 55999,
-    pictureUrl: 'https://http2.mlstatic.com/D_NQ_NP_752668-MLA42425595510_062020-O.webp',
-    stock: 5,
-    category_id:1,
-},
-{
-    id: 4,
-    title: 'Calefaccion 2',
-    description: 'Calefactor Central Conductos Goodman Gmp 125-20.750 Kcal/h',
-    price: 48590,
-    pictureUrl: 'https://http2.mlstatic.com/D_NQ_NP_752668-MLA42425595510_062020-O.webp',
-    stock: 0,
-    category_id:1,
-},
-{
-    id: 5,
-    title: 'Calefaccion 3',
-    description: 'Calefactor Central Conductos Goodman Gmp 125-28.750 Kcal/h',
-    price: 79999,
-    pictureUrl: 'https://http2.mlstatic.com/D_NQ_NP_752668-MLA42425595510_062020-O.webp',
-    stock: 4,
-    category_id:1,
-},
-{
-    id: 6,
-    title: 'Termotanque 1',
-    description: 'Termotanque eléctrico Señorial Zafiro TESZ-65 blanco 55L 230V',
-    price: 55999,
-    pictureUrl: 'https://http2.mlstatic.com/D_NQ_NP_979289-MLA42828721677_072020-O.webp',
-    stock: 5,
-    category_id:2,
-},
-{
-    id: 7,
-    title: 'Termotanque 2',
-    description: 'Termotanque eléctrico Señorial Zafiro TESZ-65 blanco 45L 230V',
-    price: 48590,
-    pictureUrl: 'https://http2.mlstatic.com/D_NQ_NP_979289-MLA42828721677_072020-O.webp',
-    stock: 0,
-    category_id:2,
-},
-{
-    id: 8,
-    title: 'Termotanque 3',
-    description: 'Termotanque eléctrico Señorial Zafiro TESZ-65 blanco 65L 230V',
-    price: 79999,
-    pictureUrl: 'https://http2.mlstatic.com/D_NQ_NP_979289-MLA42828721677_072020-O.webp',
-    stock: 4,
-    category_id:2,
-}]
+const ItemList = ({items}) => {
 
-const ItemList = (category) => {
-    
-    let newProducts;
-
-    if(category.category !== undefined){
-        newProducts = products.filter((product)=>{
-            return product.category_id === Number(category.category)
-        })
-    }else{
-        newProducts = products
-    }
-
-    const retrasoRed = new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-            resolve(newProducts)
-        }, 2000)
-    })
-
-    retrasoRed.then((newProducts)=>{
-        newProducts.map(({id})=>(
-            document.getElementById('itemCount_' + id).className = "itemCountFadeIn"
-        ))
-    })
-
-    const items = newProducts.map(({id, title, description, price, pictureUrl, stock})=>(
-        <Item initial='1' key= {id} id={id} title={title} description={description} price={price} pictureUrl={pictureUrl} stock={stock} />
-    ))
+    console.log(items.length)
 
     return (
         <div className="itemRow">
-            {items}
+            {items.length !== 0 ? items.map(item=><Item key={item.id} item={item} initial='1'/>) : <h1>Cargando</h1>}
         </div>
     )
 }
